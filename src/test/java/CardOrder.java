6bdd047c4334d9c0195d8c20fbec7b704c2b0681
irina <obesyuk@gmail.com>
2023-12-24 20:50:48 +0300

    error check

--- src/test/java/CardOrder.java
+++ src/test/java/CardOrder.java
@@ -3,11 +3,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class CardOrder {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CardOrder {
 
     WebDriver driver;
 
@@ -34,11 +38,22 @@ public class CardOrder {
     }
 
     @Test
-    void CardOrder() {
+    void CardOrderTest() {
 
-        driver.get("http://0.0.0.0:9999");
-//        Thread.sleep(5000);
+        driver.get("http://0.0.0.0:9999/");
 
+        driver.findElement(By.cssSelector("[data-test-id=name] input__control")).sendKeys("Иван Иванов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input__control")).sendKeys("+79007007007");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[button]")).click();
+        String text = driver.findElement(By.className("order-success")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
 
+        driver.findElement(By.cssSelector("[data-test-id=name] input_invalid")).sendKeys("Ivan Ivanov");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input__control")).sendKeys("79007007007");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[button]")).click();
+        text = driver.findElement(By.className("[data-test-id=name] input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
     }
 }
